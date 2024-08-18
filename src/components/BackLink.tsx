35c894d14addb09184ae4dd6f3ff2214c945f53a
SaloJovanovic <aleksandarjovanovic670@gmail.com>
2024-08-18 21:16:20 +0200

    promenjen link

--- src/components/BackLink.tsx
+++ src/components/BackLink.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 
-// const link = "https://web-production-e2db.up.railway.app";
-const link = "http://localhost:8080";
+const link = "https://web-production-e2db.up.railway.app";
+// const link = "http://localhost:8080";
 //https://web-production-08b6.up.railway.app
 export default link;
